7ce8f90aae5eded55371d197295e3825b61e79ec
Nicola Di Giorgio <ndg@pregiotek.com>
2016-09-29 22:00:12 +0200

    Added minification
    added min files

---
+++ Gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+        gutil = require('gulp-util'),
+        uglify = require('gulp-uglify'),
+        concat  = require('gulp-concat')
+        cleanCSS = require('gulp-clean-css');
+
+gulp.task('minimize', function () {
+    gulp.src(['./dist/*.js'])
+            .pipe(uglify())
+    .pipe(concat('ion-floating-menu.min.js'))
+            .pipe(gulp.dest('dist/'));
+
+    gulp.src(['./dist/*.css'])
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(concat('ion-floating-menu.min.css'))
+            .pipe(gulp.dest('dist'))
+});
+
+/*gulp.watch('./js/*', function () {
+ gulp.run('js');
+ });*/
+
+gulp.task('default', ['minimize'], function () {
+
+});
